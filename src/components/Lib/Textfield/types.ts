e1d7679f87bad270718ccf9417acd1d73d6e4b42
Pradip Bhusnar <bhusnarpradip@gmail.com>
2023-05-18 21:30:06 +0530

    PSB | added type prop

--- src/components/Lib/Textfield/types.ts
+++ src/components/Lib/Textfield/types.ts
@@ -4,6 +4,7 @@ export type Props = {
     label: string;
     name?: string;
     value: any;
+    type?: string;
     onChange: (e: ChangeEvent<HTMLInputElement>) => void;
     placeholder?: string;
     disabled?: boolean;
